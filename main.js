7dd0929d6509749035daf107c5f6922b8ab81b51
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-06 02:10:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const { app, BrowserWindow } = require("electron");
 
+// Defines the main application window
 let mainWindow;
 
+// Creates the main application window that will be opened during execution
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: "Image Shrink",
     width: 500,
     height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  // Links the HTML file to the JS file so that the application will have displayed content
+  mainWindow.loadFile("./app/index.html");
 }
 
+// Opens/Creates the window that displays the application
 app.on("ready", createMainWindow);
