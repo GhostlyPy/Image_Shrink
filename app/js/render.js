fabcac5766cad6d32db8acd9d813e9b42b8f36c6
trevor_borden@outlook.com <56496067+GhostlyPy@users.noreply.github.com>
2020-07-06 05:49:28 -0500

    Update render.js

--- app/js/render.js
+++ app/js/render.js
@@ -1,2 +1,26 @@
 const path = require('path');
 const os = require('os');
+const {
+    ipcRenderer
+} = require('electron')
+
+// Retrieves the form image, slider, and image identifers
+const form = document.getElementById('image-form');
+const slider = document.getElementById('slider');
+const img = document.getElementById('img');
+
+// Outputs the path of the applications home directory
+document.getElementById('output-path').innerText = path.join(os.homedir(), 'imageshrink');
+
+// onSubmit
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const imgPath = img.files[0].path;
+    const quality = slider.value;
+
+    ipcRenderer.send('image:minimize', {
+        imgPath,
+        quality,
+    })
+});
