ad5167a871d2fc153cf6457ff9b87a5aeec6f6fe
GhostlyPy <56496067+GhostlyPy@users.noreply.github.com>
2021-06-08 10:07:33 -0500

    fixed comment syntax

--- main.js
+++ main.js
@@ -158,9 +158,7 @@ async function shrinkImage({ imgPath, quality, dest }) {
   }
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+/* Quit when all windows are closed, except on macOS. There, it's common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q. */
 app.on("window-all-closed", () => {
   if (!isMac) {
     app.quit();
@@ -168,8 +166,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  /* On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open. */
   if (BrowserWindow.getAllWindows().length === 0) {
     createMainWindow();
   }
